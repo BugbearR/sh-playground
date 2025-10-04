afbe35204fc58011d8f63989a2a340a4c521b5c9
BugbearR <bugbearr@bugbearr.jp>
2025-10-04 11:09:18 +0900

    Add posix_sample scripts for here documents

---
+++ here_document/posix_sample/posix_sample2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# see https://pubs.opengroup.org/onlinepubs/9799919799/utilities/V3_chap02.html#tag_19_07_04
+cat <<eof1 >o1.txt; cat <<eof2 >o2.txt
+Hi,
+eof1
+Helene.
+eof2
---
+++ here_document/posix_sample/with_tab.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# see https://pubs.opengroup.org/onlinepubs/9799919799/utilities/V3_chap02.html#tag_19_07_04
+cat <<-eof1; cat <<-eof2
+	Hi,
+eof1
+	Helene.
+eof2
